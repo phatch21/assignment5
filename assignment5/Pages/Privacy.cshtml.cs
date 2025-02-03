bb1de617b23ff2966ef83311518a5b8ba490437b
phatch21 <phatch21@byu.edu>
2025-02-03 13:18:17 -0700

    Add project files.

---
+++ assignment5/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace assignment5.Pages
+{
+    public class CalcModel : PageModel
+    {
+        private readonly ILogger<CalcModel> _logger;
+
+        public CalcModel(ILogger<CalcModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
